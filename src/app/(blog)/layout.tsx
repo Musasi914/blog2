d8b70c4075a2e20b92f5bfc73dd27813d4682665
Hidetaka Matsuda <sopmod120@gmail.com>
2025-09-18 15:18:57 +0900

    name change

--- src/app/(blog)/layout.tsx
+++ src/app/(blog)/layout.tsx
@@ -11,10 +11,10 @@ const zenKaku = Zen_Kaku_Gothic_New({
 
 export const metadata: Metadata = {
   title: {
-    default: "福沢リョウの日記帳",
-    template: `%s | 福沢リョウの日記帳`,
+    default: "福沢コウの日記帳",
+    template: `%s | 福沢コウの日記帳`,
   },
-  description: "福沢リョウの自分振り帰り用途のサイトです。",
+  description: "福沢コウの自分振り帰り用途のサイトです。",
   alternates: {
     canonical: "https://blog2-one-phi.vercel.app/",
   },
@@ -31,30 +31,19 @@ export default function BlogLayout({
         <div className="font-zengothic grid grid-rows-[1fr_auto] min-h-screen w-full">
           <header className="py-2 mx-auto fixed bottom-0 sm:bottom-auto sm:top-0 sm:backdrop-blur-xl w-full bg-background sm:bg-inherit border-t sm:border-none border-customgray z-10">
             <nav className="grid grid-cols-3 sm:flex justify-center gap-5 mx-auto w-11/12 max-w-screen-xl uppercase">
-              <Link
-                href={"/"}
-                className="grid place-items-center p-2 hover:opacity-80"
-              >
+              <Link href={"/"} className="grid place-items-center p-2 hover:opacity-80">
                 blog
               </Link>
-              <Link
-                href={"/labo"}
-                className="grid place-items-center p-2 hover:opacity-80"
-              >
+              <Link href={"/labo"} className="grid place-items-center p-2 hover:opacity-80">
                 labo
               </Link>
-              <Link
-                href={"/about"}
-                className="grid place-items-center p-2 hover:opacity-80"
-              >
+              <Link href={"/about"} className="grid place-items-center p-2 hover:opacity-80">
                 about
               </Link>
             </nav>
           </header>
           <main>{children}</main>
-          <footer className="py-3 text-center mb-14 sm:mb-0">
-            &copy; 2025 Ryo Fukuzawa Diary
-          </footer>
+          <footer className="py-3 text-center mb-14 sm:mb-0">&copy; 2025 Koh Fukuzawa Diary</footer>
         </div>
       </body>
     </html>
