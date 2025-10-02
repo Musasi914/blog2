82fcec4c8c16bb1f3eb265e407c39cc423c9356d
Hidetaka Matsuda <sopmod120@gmail.com>
2025-10-02 23:21:30 +0900

    googleAnalytics追加

--- src/app/(blog)/layout.tsx
+++ src/app/(blog)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "../globals.css";
 import Link from "next/link";
 import { Zen_Kaku_Gothic_New } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const zenKaku = Zen_Kaku_Gothic_New({
   weight: ["300", "400", "700"],
@@ -31,21 +32,33 @@ export default function BlogLayout({
         <div className="font-zengothic grid grid-rows-[1fr_auto] min-h-screen w-full">
           <header className="py-2 mx-auto fixed bottom-0 sm:bottom-auto sm:top-0 sm:backdrop-blur-xl w-full bg-background sm:bg-inherit border-t sm:border-none border-customgray z-10">
             <nav className="grid grid-cols-3 sm:flex justify-center gap-5 mx-auto w-11/12 max-w-screen-xl uppercase">
-              <Link href={"/"} className="grid place-items-center p-2 hover:opacity-80">
+              <Link
+                href={"/"}
+                className="grid place-items-center p-2 hover:opacity-80"
+              >
                 blog
               </Link>
-              <Link href={"/labo"} className="grid place-items-center p-2 hover:opacity-80">
+              <Link
+                href={"/labo"}
+                className="grid place-items-center p-2 hover:opacity-80"
+              >
                 labo
               </Link>
-              <Link href={"/about"} className="grid place-items-center p-2 hover:opacity-80">
+              <Link
+                href={"/about"}
+                className="grid place-items-center p-2 hover:opacity-80"
+              >
                 about
               </Link>
             </nav>
           </header>
           <main>{children}</main>
-          <footer className="py-3 text-center mb-14 sm:mb-0">&copy; 2025 Koh Fukuzawa Diary</footer>
+          <footer className="py-3 text-center mb-14 sm:mb-0">
+            &copy; 2025 Koh Fukuzawa Diary
+          </footer>
         </div>
       </body>
+      <GoogleAnalytics gaId="G-033R4YB0LF" />
     </html>
   );
 }
